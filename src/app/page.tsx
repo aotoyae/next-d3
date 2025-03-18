75362a38564e71a8519291509727448275030f7e
kimsohyeon <aotoyae@gmail.com>
2025-03-18 23:13:51 +0900

    delete: page 내 사용하지 않는 컴포넌트 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center">
       <h1 className="py-4 text-3xl font-black">A2A Network Graph View</h1>
-      {/* <NetworkGraph /> */}
-      {/* <GalaxyGraph /> */}
       <AToAGraph />
-      {/* <BarChart data={data} /> */}
     </div>
   );
 }
