4bf32b4b60d74451be6c950e9dd4daf4343e147d
kimsohyeon <aotoyae@gmail.com>
2025-03-18 23:06:23 +0900

    feat: title 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import GalaxyGraph from '@/components/GalaxyGraph';
 export default function Home() {
   return (
     <div className="flex flex-col items-center">
-      {/* <h1 className="py-4 text-3xl font-black">D3.js with Next.js</h1> */}
+      <h1 className="py-4 text-3xl font-black">A2A Network Graph View</h1>
       {/* <NetworkGraph /> */}
       {/* <GalaxyGraph /> */}
       <AToAGraph />
